ab8e5f8b3ca35958cc9b2955b2086d2e9cef1bbd
Soubhik Samanta <soubhiksamanta25@gmail.com>
2025-04-30 00:45:29 +0530

    add user model

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,10 @@
 import express from 'express'
+import dotenv from 'dotenv'
+
 import authRoutes from './routes/auth.routes.js'
+import {connectDB} from './lib/db.js'
+
+dotenv.config()
 const app =express();
 
 app.use("/api/auth",authRoutes)
@@ -7,4 +12,5 @@ app.use("/api/auth",authRoutes)
 
 app.listen(process.env.PORT || 5001,()=>{
     console.log(`server is running on port ${process.env.PORT || 5001}`);
+    connectDB();
 }) 
