9f0c94bcd17e7d066f813ff542001dcbd40e3dde
Soubhik Samanta <soubhiksamanta25@gmail.com>
2025-05-02 01:49:42 +0530

    feat: implement user authentication features with login, logout, and profile update functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,8 @@ import SignUpPage from './pages/SignUpPage'
 import LoginPage from './pages/LoginPage'
 import SettingsPage from './pages/SettingsPage'
 import ProfilePage from './pages/ProfilePage'
-import { Navbar } from './components/Navbar'
+import Navbar from './components/Navbar'
+
 
 
 
